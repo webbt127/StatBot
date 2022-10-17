f0851b6048b0c40e710695ddf993d56ba6846b2d
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-16 21:30:57 -0500

    Update func_price_klines.py

--- func_price_klines.py
+++ func_price_klines.py
@@ -35,7 +35,7 @@ def get_price_klines(symbol):
     time.sleep(0.1)
 
     # Return output
-    print(prices)
-    return prices["bars"]
+    print(price["Bar"])
+    return prices["Bar"]
 
 get_price_klines('TSLA')
