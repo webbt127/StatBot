050ba2b901c41a11a1694ba731cee6a94996f71e
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-16 20:45:40 -0500

    Update func_price_klines.py

--- func_price_klines.py
+++ func_price_klines.py
@@ -17,23 +17,23 @@ if timeframe == 60:
     time_start_date = datetime.datetime.now() - datetime.timedelta(hours=kline_limit)
 if timeframe == "D":
     time_start_date = datetime.datetime.now() - datetime.timedelta(days=kline_limit)
-time_start_seconds = int(time_start_date.timestamp())
+time_start = time_start_date.isoformat()
 
 # Get historical prices (klines)
 def get_price_klines(symbol):
 
     # Get prices
-    prices = session.query_mark_price_kline(
+    prices = session.get_bars(
         symbol = symbol,
         timeframe = Timeframe.Hour,
         limit = kline_limit,
-        from_time = time_start_seconds
+        from_time = time_start
     )
 
     # Manage API calls
     time.sleep(0.1)
 
     # Return output
-    if len(prices["result"]) != kline_limit:
+    if len(prices["bars"]) != kline_limit:
         return []
-    return prices["result"]
+    return prices["bars"]
