176f2cb4ccdfeaede595d9d220809cdcce7cd83a
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-16 21:08:29 -0500

    Update func_get_symbols.py

--- func_get_symbols.py
+++ func_get_symbols.py
@@ -5,10 +5,8 @@ def get_tradeable_symbols():
 
     # Get available symbols
     symbols = session.list_assets(status='active')
-    sym_list = [a for a in symbols if a.easy_to_borrow == True and a.tradable == True and a.exchange == "IEX"]
+    sym_list = [a for a in symbols if a.easy_to_borrow == True and a.tradable == True]
 
     # Return ouput
     print(sym_list)
     return sym_list
-
-get_tradeable_symbols()
