3342c57c8e5d53bb35554dbfe08e1184b48c5637
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-17 19:43:27 -0500

    Update func_prices_json.py

--- func_prices_json.py
+++ func_prices_json.py
@@ -10,7 +10,7 @@ def get_price_history(symbols):
     counts = 0
     price_history_dict = {}
     with alive_bar(len(symbols)) as bar:
-        for sym in symbols:
+        for sym in symbols and counts < 200:
             symbol_name = sym.symbol
             price_history = get_price_klines(symbol_name)
             if price_history is not None:
