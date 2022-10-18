afe9586b73f29d7331b6fd58f5fdc3646cef9d36
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-17 19:40:39 -0500

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -2,7 +2,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from func_get_symbols import get_tradeable_symbols
-from func_prices_json import store_price_history
+from func_prices_json import get_price_history
 #from func_cointegration import get_cointegrated_pairs
 #from func_plot_trends import plot_trends
 import pandas as pd
@@ -22,14 +22,12 @@ if __name__ == "__main__":
     # # STEP 2 - Construct and save price history
      print("Constructing and saving price data to JSON...")
      if len(sym_response) > 0:
-         store_price_history(sym_response)
+         price_dict = get_price_history(sym_response)
 
     # # STEP 3 - Find Cointegrated pairs
-    # print("Calculating co-integration...")
-    # with open("1_price_list.json") as json_file:
-    #     price_data = json.load(json_file)
-    #     if len(price_data) > 0:
-    #         coint_pairs = get_cointegrated_pairs(price_data)
+     print("Calculating co-integration...")
+         if len(price_dict) > 0:
+             coint_pairs = get_cointegrated_pairs(price_dict)
 
     # STEP 4 - Plot trends and save for backtesting
     #print("Plotting trends...")
