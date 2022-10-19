9ed9254d564bcd6633e48a0834a7e1e997b60f61
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-18 23:48:27 -0500

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -1,9 +1,9 @@
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-from func_get_symbols import get_tradeable_symbols
-from func_prices_json import get_price_history
-from func_cointegration import get_cointegrated_pairs
+from func_get_symbols import *
+from func_prices_json import *
+from func_cointegration import *
 #from func_plot_trends import plot_trends
 import pandas as pd
 import json
@@ -16,15 +16,15 @@ if __name__ == "__main__":
     
 
     # # STEP 1 - Get list of symbols
-    print("Getting symbols...")
-    sym_response = get_tradeable_symbols()
+    lg.info("Getting symbols...")
+    asset_list = get_tradeable_symbols()
 
     # # STEP 2 - Construct and save price history
-    print("Constructing and saving price data to JSON...")
-    if len(sym_response) > 0:
-        price_dict = get_price_history(sym_response)
+    lg.info("Constructing and saving price data to JSON...")
+    if len(asset_list) > 0:
+        get_price_history(asset_list)
 
     # # STEP 3 - Find Cointegrated pairs
-    print("Calculating co-integration...")
-    if len(price_dict) > 0:
-        coint_pairs = get_cointegrated_pairs(price_dict)
+    lg.info("Calculating co-integration...")
+    if len(asset_list) > 0:
+        coint_pairs = get_cointegrated_pairs(asset_list)
