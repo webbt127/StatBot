6b3220674536d918735aadae1b0cbe8fbc20fe24
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-18 23:53:55 -0500

    Update func_get_symbols.py

--- func_get_symbols.py
+++ func_get_symbols.py
@@ -6,7 +6,6 @@ def get_tradeable_symbols():
     # Get available symbols
     active_assets = session.list_assets(status='active')
     asset_list = [a for a in active_assets if a.easy_to_borrow == True and a.tradable == True and getattr(a, 'class') == 'us_equity']
-    print(asset_list)
 
     # Return ouput
     return asset_list
