54f4b17597359b8fe3b28bf3cd78b461d7a29424
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-18 21:56:19 -0500

    Update func_order_review.py

--- func_order_review.py
+++ func_order_review.py
@@ -2,7 +2,7 @@ from func_position_calls import query_existing_order
 from func_position_calls import get_open_positions
 from func_position_calls import get_active_positions
 from func_calcultions import get_trade_details
-from config_ws_connect import ws_public
+#from config_ws_connect import ws_public
 
 
 # Check order items
