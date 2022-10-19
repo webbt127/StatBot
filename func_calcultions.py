2c6d3f77149c714ac62779436e6bc0211d2f6aee
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-18 23:12:12 -0500

    Update func_calcultions.py

--- func_calcultions.py
+++ func_calcultions.py
@@ -16,45 +16,28 @@ def get_trade_details(orderbook, direction="Long", capital=0):
     mid_price = 0
     quantity = 0
     stop_loss = 0
-    bid_items_list = []
-    ask_items_list = []
 
     # Get prices, stop loss and quantity
     if orderbook:
 
         # Set price rounding
-        price_rounding = rounding_ticker_1 if orderbook[0]["symbol"] == ticker_1 else rounding_ticker_2
-        quantity_rounding = quantity_rounding_ticker_1 if orderbook[0]["symbol"] == ticker_1 else quantity_rounding_ticker_2
-
-        # Organise prices
-        for level in orderbook:
-            if level["side"] == "Buy":
-                bid_items_list.append(float(level["price"]))
-            else:
-                ask_items_list.append(float(level["price"]))
-
-        # Calculate price, size, stop loss and average liquidity
-        if len(ask_items_list) > 0 and len(bid_items_list) > 0:
-
-            # Sort lists
-            ask_items_list.sort()
-            bid_items_list.sort()
-            bid_items_list.reverse()
+        price_rounding = rounding_ticker_1 if orderbook.symbol == ticker_1 else rounding_ticker_2
+        quantity_rounding = quantity_rounding_ticker_1 if orderbook.symbol == ticker_1 else quantity_rounding_ticker_2
 
             # Get nearest ask, nearest bid and orderbook spread
-            nearest_ask = ask_items_list[0]
-            nearest_bid = bid_items_list[0]
+        nearest_ask = orderbook.ap
+        nearest_bid = orderbook.bp
 
             # Calculate hard stop loss
-            if direction == "Long":
-                mid_price = nearest_bid # placing at Bid has high probability of not being cancelled, but may not fill
-                stop_loss = round(mid_price * (1 - stop_loss_fail_safe), price_rounding)
-            else:
-                mid_price = nearest_ask  # placing at Ask has high probability of not being cancelled, but may not fill
-                stop_loss = round(mid_price * (1 + stop_loss_fail_safe), price_rounding)
+        if direction == "Long":
+            mid_price = nearest_bid # placing at Bid has high probability of not being cancelled, but may not fill
+            stop_loss = round(mid_price * (1 - stop_loss_fail_safe), price_rounding)
+        else:
+            mid_price = nearest_ask  # placing at Ask has high probability of not being cancelled, but may not fill
+            stop_loss = round(mid_price * (1 + stop_loss_fail_safe), price_rounding)
 
             # Calculate quantity
-            quantity = round(capital / mid_price, quantity_rounding)
+        quantity = round(capital / mid_price, quantity_rounding)
 
     # Output results
     return (mid_price, stop_loss, quantity)
