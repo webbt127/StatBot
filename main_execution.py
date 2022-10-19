252fc494877dd0bf2ba2aba886ce8780e2a5465b
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-18 22:02:01 -0500

    Update main_execution.py

--- main_execution.py
+++ main_execution.py
@@ -23,76 +23,76 @@ initialise_order_execution('TSLA', "Long", 1000)
 if __name__ == "__main__":
 
     # Initial printout
-    print("StatBot initiated...")
+	print("StatBot initiated...")
 
     # Initialise variables
-    status_dict = {"message": "starting..."}
-    order_long = {}
-    order_short = {}
-    signal_sign_positive = False
-    signal_side = ""
-    kill_switch = 0
+	status_dict = {"message": "starting..."}
+	order_long = {}
+	order_short = {}
+	signal_sign_positive = False
+	signal_side = ""
+	kill_switch = 0
 
     # Save status
-    save_status(status_dict)
+	save_status(status_dict)
 
     # Set leverage in case forgotten to do so on the platform
-    print("Setting leverage...")
-    set_leverage(signal_positive_ticker)
-    set_leverage(signal_negative_ticker)
+	print("Setting leverage...")
+	set_leverage(signal_positive_ticker)
+	set_leverage(signal_negative_ticker)
 
     # Commence bot
-    print("Seeking trades...")
-    while True:
+	print("Seeking trades...")
+	while True:
 
         # Keep alive
-        pinged = ws_public.fetch(subs_public[0])
+		pinged = ws_public.fetch(subs_public[0])
 
         # Pause - protect API
-        time.sleep(3)
+		time.sleep(3)
 
         # Check if open trades already exist
-        is_p_ticker_open = open_position_confirmation(signal_positive_ticker)
-        is_n_ticker_open = open_position_confirmation(signal_negative_ticker)
-        is_p_ticker_active = active_position_confirmation(signal_positive_ticker)
-        is_n_ticker_active = active_position_confirmation(signal_negative_ticker)
-        checks_all = [is_p_ticker_open, is_n_ticker_open, is_p_ticker_active, is_n_ticker_active]
-        is_manage_new_trades = not any(checks_all)
+		is_p_ticker_open = open_position_confirmation(signal_positive_ticker)
+		is_n_ticker_open = open_position_confirmation(signal_negative_ticker)
+		is_p_ticker_active = active_position_confirmation(signal_positive_ticker)
+		is_n_ticker_active = active_position_confirmation(signal_negative_ticker)
+		checks_all = [is_p_ticker_open, is_n_ticker_open, is_p_ticker_active, is_n_ticker_active]
+		is_manage_new_trades = not any(checks_all)
 
         # Save status
-        status_dict["message"] = "Initial checks made..."
-        status_dict["checks"] = checks_all
-        save_status(status_dict)
+		status_dict["message"] = "Initial checks made..."
+		status_dict["checks"] = checks_all
+		save_status(status_dict)
 
         # Check for signal and place new trades
-        if is_manage_new_trades and kill_switch == 0:
-            status_dict["message"] = "Managing new trades..."
-            save_status(status_dict)
-            kill_switch, signal_side = manage_new_trades(kill_switch)
+		if is_manage_new_trades and kill_switch == 0:
+			status_dict["message"] = "Managing new trades..."
+			save_status(status_dict)
+			kill_switch, signal_side = manage_new_trades(kill_switch)
 
         # Managing open kill switch if positions change or should reach 2
         # Check for signal to be false
-        if kill_switch == 1:
+		if kill_switch == 1:
 
             # Get and save the latest z-score
-            zscore, signal_sign_positive = get_latest_zscore()
+			zscore, signal_sign_positive = get_latest_zscore()
 
             # Close positions
-            if signal_side == "positive" and zscore < 0:
-                kill_switch = 2
-            if signal_side == "negative" and zscore >= 0:
-                kill_switch = 2
+			if signal_side == "positive" and zscore < 0:
+				kill_switch = 2
+			if signal_side == "negative" and zscore >= 0:
+				kill_switch = 2
 
             # Put back to zero if trades are closed
-            if is_manage_new_trades and kill_switch != 2:
-                kill_switch = 0
+			if is_manage_new_trades and kill_switch != 2:
+				kill_switch = 0
 
         # Close all active orders and positions
-        if kill_switch == 2:
-            print("Closing all positions...")
-            status_dict["message"] = "Closing existing trades..."
-            save_status(status_dict)
-            kill_switch = close_all_positions(kill_switch)
+		if kill_switch == 2:
+			print("Closing all positions...")
+			status_dict["message"] = "Closing existing trades..."
+			save_status(status_dict)
+			kill_switch = close_all_positions(kill_switch)
 
             # Sleep for 5 seconds
-            time.sleep(5)
+			time.sleep(5)
