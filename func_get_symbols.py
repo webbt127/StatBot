5f614fa0df6adbe0618ada99f08b42a0b90e0c14
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-18 23:31:51 -0500

    Update func_get_symbols.py

--- func_get_symbols.py
+++ func_get_symbols.py
@@ -4,8 +4,8 @@ from config_strategy_api import session
 def get_tradeable_symbols():
 
     # Get available symbols
-    symbols = session.list_assets(status='active')
-    sym_list = [a for a in symbols if a.easy_to_borrow == True and a.tradable == True and getattr(a, 'class') == 'us_equity']
+    active_assets = session.list_assets(status='active')
+    asset_list = [a for a in active_assets if a.easy_to_borrow == True and a.tradable == True and getattr(a, 'class') == 'us_equity']
 
     # Return ouput
-    return sym_list
+    return asset_list
