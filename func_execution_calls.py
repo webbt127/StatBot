a4d95c2c58ef957f032c3a79618b3ada3ddac46d
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-18 22:57:25 -0500

    Update func_execution_calls.py

--- func_execution_calls.py
+++ func_execution_calls.py
@@ -48,7 +48,7 @@ def place_order(ticker, price, quantity, direction, stop_loss):
 # Initialise execution
 def initialise_order_execution(ticker, direction, capital):
     orderbook = session_private.get_latest_quote(ticker)
-    data = getattr('as')
+    data = getattr(orderbook, 'as')
     print(data)
     if orderbook:
         mid_price, stop_loss, quantity = get_trade_details(orderbook, direction, capital)
