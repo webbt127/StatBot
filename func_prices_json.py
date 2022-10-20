d7577e0fc7aa20fdf4192d492360b39ada06fe86
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-19 19:14:43 -0500

    Update func_prices_json.py

--- func_prices_json.py
+++ func_prices_json.py
@@ -4,14 +4,14 @@ from alive_progress import alive_bar
 import logging as lg
 
 # Store price histry for all available pairs
-def get_price_history(asset_list):
+def get_price_history(asset_list, api):
 
     # Get prices and store in DataFrame
 	counts = 0
 	price_history_dict = {}
 	with alive_bar(len(asset_list)) as bar:
 		for asset in asset_list:
-			get_price_klines(asset)
+			get_price_klines(asset, api)
 			if asset.klines is not None:
 				lg.info("Successfully Stored Data For %s!" % asset.symbol)
 			else:
