2d14e3be61aa74eba5654b192712feb13a23e342
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-19 19:33:37 -0500

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -11,19 +11,22 @@ import json
 from logger import *
 
 initialize_logger()
+
+"""STRATEGY CODE"""
+if __name__ == "__main__":
     
 
     # # STEP 1 - Get list of symbols
-lg.info("Getting symbols...")
-asset_list = get_tradeable_symbols()
+    lg.info("Getting symbols...")
+    asset_list = get_tradeable_symbols()
 
     # # STEP 2 - Construct and save price history
-lg.info("Constructing and saving price data to JSON...")
-if len(asset_list) > 0:
-	get_price_history(asset_list, api)
+    lg.info("Constructing and saving price data to JSON...")
+    if len(asset_list) > 0:
+        get_price_history(asset_list, api)
 
     # # STEP 3 - Find Cointegrated pairs
-lg.info("Calculating co-integration...")
-if len(asset_list) > 0:
-	coint_pairs = get_cointegrated_pairs(asset_list)
-	print(coint_pairs)
+    lg.info("Calculating co-integration...")
+    if len(asset_list) > 0:
+        coint_pairs = get_cointegrated_pairs(asset_list)
+        print(coint_pairs)
