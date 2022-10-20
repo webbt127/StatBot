980cb057f65a6312b224ecd179f5d53211c5bc2a
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-19 20:49:21 -0500

    Update func_cointegration.py

--- func_cointegration.py
+++ func_cointegration.py
@@ -61,7 +61,7 @@ def get_cointegrated_pairs(asset_list):
 					sorted_characters = sorted(sym_1.symbol + sym_2.symbol)
 					unique = "".join(sorted_characters)
 					if unique not in included_list:
-						if sym_1.klines is not None and sym_2.klines is not None:
+						if 'close' sym_1.klines and 'close' in sym_2.klines:
 							print(sym_1.klines)
 							sym_1.close_series = extract_close_prices(sym_1)
 							sym_2.close_series = extract_close_prices(sym_2)
