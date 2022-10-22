f8454fcb8dc18804b22fb63837b1c9f8805e3e60
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-22 13:03:29 -0500

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     # # STEP 2 - Construct and save price history
 	lg.info("Constructing and saving price data to JSON...")
-	get_price_history(asset_list)
+	get_price_history()
 
     # # STEP 3 - Find Cointegrated pairs
 	lg.info("Calculating co-integration...")
