f6e4dae40cbe079febb8d386adb0b22e77b327b0
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-22 13:04:12 -0500

    Update func_prices_json.py

--- func_prices_json.py
+++ func_prices_json.py
@@ -7,7 +7,7 @@ import logging as lg
 from joblib import Parallel, delayed, parallel_backend
 
 # Store price histry for all available pairs
-def get_price_history(asset_list):
+def get_price_history():
 
     # Get prices and store in DataFrame
 	price_history_dict = {}
@@ -24,6 +24,6 @@ def price_history_execution(asset):
 	if asset.klines is not None:
 		lg.info("Successfully Stored Data For %s!" % asset.symbol)
 	else:
-		#asset_list.remove(asset)
+		asset_list.remove(asset)
 		lg.info("Unable To Store Data For %s! Removed From Asset List" % asset.symbol)
 	return asset
