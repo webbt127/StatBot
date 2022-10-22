56849a06ab4129372c601d4dbf0a2d5c9c471798
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-22 11:57:56 -0500

    Update func_get_symbols.py

--- func_get_symbols.py
+++ func_get_symbols.py
@@ -1,7 +1,7 @@
 from config_strategy_api import *
 
 # Get symbols that are tradeable
-def get_tradeable_symbols(api):
+def get_tradeable_symbols():
 
     # Get available symbols
     active_assets = api.session.list_assets(status='active')
