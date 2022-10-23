711ab8effe86f063d9371f7b719c88c8d15d7eb5
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-23 17:03:05 -0500

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
         
     # # STEP 4
 	while 1:
-		for i in coint_pairs['sym_1']
+		for i in coint_pairs['sym_1']:
 			if i < api.max_positions:
 				position_1 = position()
 				position_1.symbol = coint_pairs['sym_1'][i]
