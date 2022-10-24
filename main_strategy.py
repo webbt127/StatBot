0c93b97b9d8d288bcf91889398787f369cfe5f33
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-23 22:13:58 -0500

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -50,6 +50,7 @@ if __name__ == "__main__":
     # # STEP 4
 	while 1:
 		for i in coint_pairs['sym_1']:
+			print(i)
 			position_1 = position()
 			position_1.symbol = coint_pairs['sym_1'][i]
 			position_2 = position()
