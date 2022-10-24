2525f525770889458e807bfdf71731a9831c11b6
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-23 22:26:44 -0500

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
 			get_ticker_position(position_1)
 			get_ticker_position(position_2)
 	
-			if position_1.qty != 0 and position_2.qty != 0:
+			if position_1.qty == 0 and position_2.qty == 0:
 				is_manage_new_trades = True
 			else:
 				is_manage_new_trades = False
