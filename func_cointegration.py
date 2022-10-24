32848caad3fba1d4bb9dc65c78ce874e9ef70b1f
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-23 23:05:58 -0500

    Update func_cointegration.py

--- func_cointegration.py
+++ func_cointegration.py
@@ -92,32 +92,32 @@ def check_pairs(sym_1, sym_2):
 def get_latest_zscore(position_1, position_2):
 
     # Get latest price history
-    get_price_klines(position_1)
-    get_price_klines(position_2)
+	get_price_klines(position_1)
+	get_price_klines(position_2)
 	initialize_order_execution(position_1, api.capital)
-    initialize_order_execution(position_2, api.capital)
-    get_trade_details(position_1, api.capital)
-    get_trade_details(position_2, api.capital)
+	initialize_order_execution(position_2, api.capital)
+	get_trade_details(position_1, api.capital)
+	get_trade_details(position_2, api.capital)
 
     # Get z_score and confirm if hot
-    if len(position_1.klines) > 0 and len(position_2.klines) > 0:
+	if len(position_1.klines) > 0 and len(position_2.klines) > 0:
 
         # Replace last kline price with latest orderbook mid price
-        position_1.klines = position_1.klines[:-1]
-        position_2.klines = position_2.klines[:-1]
-        position_1.klines.append(position_1.mid_price)
-        position_2.klines.append(position_2.mid_price)
+		position_1.klines = position_1.klines[:-1]
+		position_2.klines = position_2.klines[:-1]
+		position_1.klines.append(position_1.mid_price)
+		position_2.klines.append(position_2.mid_price)
 
         # Get latest zscore
-        _, zscore_list = calculate_metrics(position_1.klines, position_2.klines)
-        zscore = zscore_list[-1]
-        if zscore > 0:
-            signal_sign_positive = True
-        else:
-            signal_sign_positive = False
+		_, zscore_list = calculate_metrics(position_1.klines, position_2.klines)
+		zscore = zscore_list[-1]
+		if zscore > 0:
+			signal_sign_positive = True
+		else:
+			signal_sign_positive = False
 
         # Return output
-        return (zscore, signal_sign_positive)
+		return (zscore, signal_sign_positive)
 
     # Return output if not true
-    return
+	return
