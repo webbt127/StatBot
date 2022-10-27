a1b52ca032fd290028963de7598cd3bba93b38a4
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-27 17:03:25 -0500

    Update func_trade_management.py

--- func_trade_management.py
+++ func_trade_management.py
@@ -16,51 +16,54 @@ def manage_new_trades(position_1, position_2):
 	position_1.close_series = extract_close_prices(position_1)
 	position_2.close_series = extract_close_prices(position_2)
 	
-	_, _, _, _, hedge_ratio, _ = calculate_cointegration(position_1, position_2)
-	spread = calculate_spread(position_1.close_series, position_2.close_series, hedge_ratio)
-	zscore_list = calculate_zscore(spread)
-	zscore = zscore_list[-1]
+	if(len(position_1.close_series) == len(position_2.close_series)):
+		_, _, _, _, hedge_ratio, _ = calculate_cointegration(position_1, position_2)
+		spread = calculate_spread(position_1.close_series, position_2.close_series, hedge_ratio)
+		zscore_list = calculate_zscore(spread)
+		zscore = zscore_list[-1]
 	
-	if zscore > 0:
-		position_1.direction = "Short"
-		position_2.direction = "Long"
-	else:
-		position_1.direction = "Long"
-		position_2.direction = "Short"
+		if zscore > 0:
+			position_1.direction = "Short"
+			position_2.direction = "Long"
+		else:
+			position_1.direction = "Long"
+			position_2.direction = "Short"
 		
-	get_mid_price(position_1)
-	get_mid_price(position_2)
+		get_mid_price(position_1)
+		get_mid_price(position_2)
 
     # Switch to hot if meets signal threshold
     # Note: You can add in coint-flag check too if you want extra vigilence
-	get_trade_details(position_1, api.tradable_capital_usdt)
-	get_trade_details(position_2, api.tradable_capital_usdt)
-	if abs(zscore) > api.signal_trigger_thresh:
+		get_trade_details(position_1, api.tradable_capital_usdt)
+		get_trade_details(position_2, api.tradable_capital_usdt)
+		if abs(zscore) > api.signal_trigger_thresh:
 
         # Get trades history for liquidity
-		get_ticker_trade_liquidity(position_2)
-		get_ticker_trade_liquidity(position_1)
+			get_ticker_trade_liquidity(position_2)
+			get_ticker_trade_liquidity(position_1)
 
         # Determine long ticker vs short ticker
-		if zscore > 0:
-			long_ticker = position_2
-			short_ticker = position_1
-			avg_liquidity_long = position_2.liquidity
-			avg_liquidity_short = position_1.liquidity
-			last_price_long = position_2.last_price
-			last_price_short = position_1.last_price
-		else:
-			long_ticker = position_1
-			short_ticker = position_2
-			avg_liquidity_long = position_1.liquidity
-			avg_liquidity_short = position_2.liquidity
-			last_price_long = position_1.last_price
-			last_price_short = position_2.last_price
+			if zscore > 0:
+				long_ticker = position_2
+				short_ticker = position_1
+				avg_liquidity_long = position_2.liquidity
+				avg_liquidity_short = position_1.liquidity
+				last_price_long = position_2.last_price
+				last_price_short = position_1.last_price
+			else:
+				long_ticker = position_1
+				short_ticker = position_2
+				avg_liquidity_long = position_1.liquidity
+				avg_liquidity_short = position_2.liquidity
+				last_price_long = position_1.last_price
+				last_price_short = position_2.last_price
 
-		initialize_order_execution(long_ticker)
-		initialize_order_execution(short_ticker)
+			initialize_order_execution(long_ticker)
+			initialize_order_execution(short_ticker)
+		else:
+			lg.info("Insufficient Zscore!")
 	else:
-		lg.info("Insufficient Zscore!")
+		lg.info("Klines have unequal length, cannot calculate cointegration!)
 
 
     # Output status
