6a58e7beee11906a565e92fbfd282767bcf7eadf
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-31 18:09:42 -0500

    Update func_cointegration.py

--- func_cointegration.py
+++ func_cointegration.py
@@ -59,7 +59,7 @@ def get_cointegrated_pairs():
 
     # Loop through coins and check for co-integration
 	with alive_bar(0, title='Checking Cointegration...') as bar:
-		Parallel(n_jobs=8, prefer="threads")(delayed(check_pairs)(sym_1, sym_2) for sym_1 in asset_list.symbols for sym_2 in asset_list.symbols)
+		Parallel(n_jobs=20, prefer="threads")(delayed(check_pairs)(sym_1, sym_2) for sym_1 in asset_list.symbols for sym_2 in asset_list.symbols)
 		df_coint = pd.DataFrame(coint_pair_list)
 		df_coint = df_coint.sort_values("zero_crossings", ascending=False)
 		df_coint = df_coint.reset_index(drop=True)
