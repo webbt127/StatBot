9bcf5fd453142e5100f60e86c5987e8600a96754
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-04 14:49:24 -0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -273,7 +273,7 @@ def get_tradeable_symbols():
 
 def filter_assets(a):
 	try:
-		a.info = yf.Tickers('TSLA', 'AAPL').info
+		a.info = yf.Tickers('TSLA', 'AAPL')
 		print(a.info)
 		a.average_volume = int(a.info['averageDailyVolume10Day'])
 	except:
