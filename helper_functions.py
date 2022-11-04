8f19b0cbbef0e41c13911f4cf4554864b36e8187
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-04 15:36:50 -0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -277,7 +277,7 @@ def filter_assets(a):
 	try:
 		#a.info = yf.Ticker(a.symbol).info
 		#a.average_volume = int(a.info['averageDailyVolume10Day'])
-		_, a.average_volume = get_klines(a, TimeFrame.Day, 1)
+		_, a.average_volume = get_price_klines(a, TimeFrame.Day, 1)
 		print(a.average_volume)
 	except Exception as e:
 		a.average_volume = 0
