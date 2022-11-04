c8c682ee1faf9065edb7f93ac423a8907ddfddc2
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-04 13:38:22 -0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -262,7 +262,7 @@ def get_tradeable_symbols():
     # Get available symbols
 	active_assets = api.session.list_assets(status='active')
 	asset_list.symbols = [a for a in active_assets if a.easy_to_borrow == True and a.tradable == True and getattr(a, 'class') == 'us_equity']
-	Parallel(n_jobs=8, prefer="threads")(delayed(filter_tradeable_symbols)(a) for a in asset_list)
+	Parallel(n_jobs=8, prefer="threads")(delayed(filter_tradeable_symbols)(a) for a in asset_list.symbols)
 	asset_list.symbols = [a for a in active_assets if a.average_volume > 1000000]
 	
 
@@ -275,7 +275,7 @@ def filter_tradeable_symbols(a):
 		a.average_volume = int(stock_info['averageDailyVolume10Day'])
 	except:
 		a.average_volume = 0
-	return
+	return a
 
 class Orderbook():
 	pass
