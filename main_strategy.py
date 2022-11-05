703126d249e5247fbc3f7d8b361b646c09ffcb37
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-05 00:23:03 -0500

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -57,19 +57,69 @@ def buy_loop():
 			if trade_complete:
 				open_position_list[trades_taken] = i
 				trades_taken = trades_taken + 1
+				added_to_list = False
+				while not added_to_list:
+					try:
+						open_position_list.acquire()
+						open_position_list.append(i)
+						open_position_list.release()
+						added_to_list = True
+					except Exception as e:
+						lg.info("Asset List is currently locked! %s" % e)
+						time.sleep(1)
 			print(open_position_list)
 				
 				
 def sell_loop():
 	while True:
-		for trade in open_position_list:
-			position_1 = position()
-			position_1.symbol = open_position_list['sym_1'][trade]
-			position_2 = position()
-			position_2.symbol = open_position_list['sym_2'][trade]
-			get_ticker_position(position_1)
-			get_ticker_position(position_2)
-		continue
+		try:
+			open_position_list.acquire()
+			open_position_list_working = open_position_list
+			open_position_list.release()
+			for trade in open_position_list_working:
+				position_1 = position()
+				position_1.symbol = trade['sym_1']
+				position_2 = position()
+				position_2.symbol = trade['sym_2']
+				get_ticker_position(position_1)
+				get_ticker_position(position_2)
+				if position_1.qty != 0 and position_2.qty != 0:
+					get_orderbook(position_1)
+					get_orderbook(position_2)
+					get_price_klines(position_1, TimeFrame.Hour, api.kline_limit)
+					get_price_klines(position_2, TimeFrame.Hour, api.kline_limit)
+					position_1.close_series = extract_close_prices(position_1)
+					position_2.close_series = extract_close_prices(position_2)
+					if(len(position_1.close_series) == len(position_2.close_series) and len(position_1.close_series) > 0):
+						_, _, _, _, hedge_ratio, _ = calculate_cointegration(position_1, position_2)
+						spread = calculate_spread(position_1.close_series, position_2.close_series, hedge_ratio)
+						zscore_list = calculate_zscore(spread)
+						zscore = zscore_list[-1]
+						if position_1.qty > 0:
+							position_1.side = 'sell'
+							position_2.side = 'buy'
+							if zscore > 0:
+								place_market_close_order(position_1)
+								place_market_close_order(position_2)
+						else:
+							position_2.side = 'sell'
+							position_1.side = 'buy'
+							if zscore < 0:
+								place_market_close_order(position_1)
+								place_market_close_order(position_2)
+					removed_from_list = False
+					while not removed_from_list:
+						try:
+							open_position_list.acquire()
+							open_position_list.remove(trade)
+							open_position_list.release()
+							removed_from_list = True
+						except Exception as e:
+							lg.info("Traded Asset List is currently locked! %s" % e)
+		except Exception as e:
+			lg.info("Traded Asset List is currently locked! %s" % e)
+			time.sleep(1)
+						
 
 """STRATEGY CODE"""
 if __name__ == "__main__":
