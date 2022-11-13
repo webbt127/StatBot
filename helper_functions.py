7fb9a853b7b1c792e0976039b0aa6aeb552fb9c7
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-13 14:15:41 -0600

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -15,77 +15,6 @@ from alpaca_trade_api.rest import TimeFrame
 import datetime
 
 
-# Manage new trade assessment and order placing
-def manage_new_trades(position_1, position_2):
-    # Get and save the latest z-score
-	get_orderbook(position_1)
-	get_orderbook(position_2)
-	
-	get_price_klines(position_1, TimeFrame.Hour, api.kline_limit)
-	get_price_klines(position_2, TimeFrame.Hour, api.kline_limit)
-	position_1.close_series = extract_close_prices(position_1)
-	position_2.close_series = extract_close_prices(position_2)
-	
-	if(len(position_1.close_series) == len(position_2.close_series) and len(position_1.close_series) > 0):
-		_, _, _, _, hedge_ratio, _ = calculate_cointegration(position_1, position_2)
-		spread = calculate_spread(position_1.close_series, position_2.close_series, hedge_ratio)
-		zscore_list = calculate_zscore(spread)
-		zscore = zscore_list[-1]
-	
-		if zscore > 0:
-			position_1.direction = "Short"
-			position_2.direction = "Long"
-		else:
-			position_1.direction = "Long"
-			position_2.direction = "Short"
-		
-		get_mid_price(position_1)
-		get_mid_price(position_2)
-
-    # Switch to hot if meets signal threshold
-    # Note: You can add in coint-flag check too if you want extra vigilence
-		get_trade_details(position_1, api.tradable_capital_usdt)
-		get_trade_details(position_2, api.tradable_capital_usdt)
-		if abs(zscore) > api.signal_trigger_thresh:
-
-        # Get trades history for liquidity
-			get_ticker_trade_liquidity(position_2)
-			get_ticker_trade_liquidity(position_1)
-
-        # Determine long ticker vs short ticker
-			if zscore > 0:
-				long_ticker = position_2
-				short_ticker = position_1
-				avg_liquidity_long = position_2.liquidity
-				avg_liquidity_short = position_1.liquidity
-				last_price_long = position_2.last_price
-				last_price_short = position_1.last_price
-			else:
-				long_ticker = position_1
-				short_ticker = position_2
-				avg_liquidity_long = position_1.liquidity
-				avg_liquidity_short = position_2.liquidity
-				last_price_long = position_1.last_price
-				last_price_short = position_2.last_price
-
-			initialize_order_execution(long_ticker)
-			initialize_order_execution(short_ticker)
-			
-			#time.sleep(60)
-			
-			get_ticker_position(long_ticker)
-			get_ticker_position(short_ticker)
-			lg.info("Pair Order Complete!")
-			
-			if long_ticker.qty != 0 and short_ticker.qty != 0:
-				return True
-			else:
-				return False
-
-
-    # Output status
-	return False
-
 def cancel_orders():
 	try:
 		api.session.cancel_all_orders()
@@ -108,53 +37,6 @@ def get_orderbook(asset):
 	asset.orderbook.bp = getattr(asset.latest_quote, 'bp')
 	return asset
 
-def get_mid_price(asset):
-	if asset.direction == "Long":
-		asset.mid_price = asset.orderbook.bp # placing at Bid has high probability of not being cancelled, but may not fill
-	else:
-		asset.mid_price = asset.orderbook.ap  # placing at Ask has high probability of not being cancelled, but may not fill
-	return asset
-
-
-# Get trade details and latest prices
-def get_trade_details(asset, capital):
-
-    # Set calculation and output variables
-	asset.price_rounding = 20
-	asset.quantity_rounding = 20
-	asset.mid_price = 0
-	asset.quantity = 0
-	asset.stop_loss = 0
-
-    # Get prices, stop loss and quantity
-	if hasattr(asset, 'orderbook'):
-
-        # Set price rounding
-		asset.price_rounding = api.rounding_ticker_1 if asset.symbol == api.ticker_1 else api.rounding_ticker_2
-		asset.quantity_rounding = api.quantity_rounding_ticker_1 if asset.symbol == api.ticker_1 else api.quantity_rounding_ticker_2
-
-            # Calculate hard stop loss
-	if asset.direction == "Long":
-		asset.mid_price = asset.orderbook.bp # placing at Bid has high probability of not being cancelled, but may not fill
-		asset.stop_loss = round(asset.mid_price * (1 - api.stop_loss_fail_safe), api.price_rounding)
-		if asset.stop_loss == 0:
-			asset.stop_loss = round(asset.close_series[0] * (1 - api.stop_loss_fail_safe), api.price_rounding)
-	else:
-		asset.mid_price = asset.orderbook.ap  # placing at Ask has high probability of not being cancelled, but may not fill
-		asset.stop_loss = round(asset.mid_price * (1 + api.stop_loss_fail_safe), api.price_rounding)
-		if asset.stop_loss == 0:
-			asset.stop_loss = round(asset.close_series[0] * (1 + api.stop_loss_fail_safe), api.price_rounding)
-
-            # Calculate quantity
-	if asset.mid_price > 0:
-		asset.quantity = round(capital / asset.mid_price)
-	elif asset.mid_price == 0:
-		asset.quantity = round(capital / asset.close_series[0])
-	else:
-		asset.quantity = 0
-
-    # Output results
-	return asset
 
 def get_price_history():
 
@@ -208,28 +90,6 @@ def get_price_klines(asset, timeframe, klines):
     # Return output
 	return asset, asset.klines
 
-def get_ticker_trade_liquidity(position):
-
-    # Get trades history
-	try:
-		trades = api.session.get_trades(symbol=position.symbol, limit=5)
-	except Exception as e:
-		lg.info("Unable to get trades %s" % e)
-
-    # Get the list for calculating the average liquidity
-	quantity_list = []
-	for trade in trades:
-		if hasattr(trade, 's'):
-			quantity_list.append(trade.s)
-
-    # Return output
-	if len(quantity_list) > 0:
-		position.liquidity = sum(quantity_list) / len(quantity_list)
-		position.last_price = round(float(trades[-1].p), 2)
-		return position
-	position.liquidity = 0
-	position.last_price = 0
-	return position
 
 def get_ticker_position(asset):
 	try:
@@ -321,28 +181,6 @@ def initialize_order_execution(asset):
 		lg.info("Quantity has not been calculated!")
 	return asset
 
-def get_position_info(asset):
-
-    # Declare output variables
-	asset.side = 0
-	asset.size = ""
-
-    # Extract position info
-	try:
-		asset.position = api.session.get_position(ticker)
-		asset.size = int(asset.position.qty)
-		if asset.size > 0:
-			asset.side = "buy"
-		else:
-			asset.side = "sell"
-	except Exception as e:
-		#lg.info("No Existing Position For %s!" % asset.symbol)
-		asset.size = 0
-		asset.side = "Sell"
-
-    # Return output
-	return asset
-
 
 #  Place market close order
 def place_market_close_order(asset):
@@ -352,7 +190,7 @@ def place_market_close_order(asset):
 		symbol=asset.symbol,
 		side=asset.side,
 		type='market',
-		qty=asset.size,
+		qty=asset.qty,
 		time_in_force="gtc"
 	)
 
