e34307b7c8093fcaf50d983ca5728648a2f98b42
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-14 18:58:57 -0600

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -183,3 +183,20 @@ def place_market_close_order(asset):
 
     # Return
 	return
+
+def match_series_lengths(position_1, position_2):
+	
+	if len(position_1.close_series) == len(position_2.close_series):
+		return position_1, position_2
+	if len(position_1.close_series) > len(position_2.close_series):
+		difference = len(position_1.close_series) - len(position_2.close_series)
+		slice = slice(difference, len(position_2.close_series, 1)
+		position_1.close_series = position_1.close_series[slice]
+		return position_1, position_2
+	if len(position_2.close_series) > len(position_1.close_series):
+		difference = len(position_2.close_series) - len(position_1.close_series)
+		slice = slice(difference, len(position_1.close_series, 1)
+		position_2.close_series = position_2.close_series[slice]
+		return position_1, position_2
+	else:
+		return position_1, position_2
