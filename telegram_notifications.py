97b69a3b2b34f6bc51c4e5cd51011846aff95ddd
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-22 14:20:34 -0600

    Update telegram_notifications.py

--- telegram_notifications.py
+++ telegram_notifications.py
@@ -7,5 +7,5 @@ def send_telegram_message(message: str, chat_id: str, api_key: str):
 	data_dict = {'chat_id': chat_id, 'text': message, 'parse_mode': 'HTML', 'disable_notification': False}
 	data = json.dumps(data_dict)
 	url = f'https://api.telegram.org/bot{api_key}/sendMessage'
-	response = requests.post(url, data=data, headers=headers, proxies=proxies, verify=False)
+	response = requests.post(url, data=data, headers=headers, verify=False)
 	return response
