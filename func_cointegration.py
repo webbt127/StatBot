d35c4d9133a66fe5e6ed83d13ee557a18929529c
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-22 17:19:18 -0600

    Update func_cointegration.py

--- func_cointegration.py
+++ func_cointegration.py
@@ -54,13 +54,15 @@ def get_cointegrated_pairs():
     # Loop through coins and check for co-integration
 	with alive_bar((len(asset_list.symbols)*len(asset_list.symbols)), title='Checking Cointegration...') as bar:
 		global included_list
-		Parallel(n_jobs=8, verbose=10, prefer="threads")(delayed(check_pairs)(sym_1, sym_2) for sym_1 in asset_list.symbols for sym_2 in asset_list.symbols)
-		df_coint = pd.DataFrame(coint_pair_list)
-		if 'zero_crossings' in df_coint:
-			df_coint = df_coint.sort_values("zero_crossings", ascending=False)
-			df_coint = df_coint.reset_index(drop=True)
-			df_coint['index'] = df_coint.index
-			df_coint.to_csv(api.pairs_path)
+		#Parallel(n_jobs=8, verbose=10, prefer="threads")(delayed(check_pairs)(sym_1, sym_2) for sym_1 in asset_list.symbols for sym_2 in asset_list.symbols)
+		for sym_1 in asset_list.symbols:
+			for sym_2 in asset_list.symbols:
+				df_coint = pd.DataFrame(coint_pair_list)
+				if 'zero_crossings' in df_coint:
+					df_coint = df_coint.sort_values("zero_crossings", ascending=False)
+					df_coint = df_coint.reset_index(drop=True)
+					df_coint['index'] = df_coint.index
+					df_coint.to_csv(api.pairs_path)
 		return df_coint
 	
 def check_pairs(sym_1, sym_2):
