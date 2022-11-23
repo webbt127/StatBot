ff31fbe284092fac011afb42844ebfa50706deee
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-22 22:46:39 -0600

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -16,16 +16,6 @@ import atexit
 
 initialize_logger()
 
-def exit_handler():
-	message = 'Exception Occurred'
-	send_telegram_message(message, api.telegram_chat_id, api.telegram_api_key)
-	return
-
-class position_list:
-	def __init__(self):
-		self.lock = Lock()
-		self.positions = pd.DataFrame(columns=['sym_1', 'sym_2', 'p_value', 't_value', 'c_value', 'hedge_ratio', 'zero_crossings', 'index', 'hedge_ratio'])
-
 atexit.register(exit_handler)
 global open_position_list
 open_position_list = position_list()
