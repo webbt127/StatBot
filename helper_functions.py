35f37b556006b937fa20463d1f2c40af12e56c9e
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-23 15:17:33 -0600

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -200,7 +200,7 @@ def add_asset(coint_pairs, open_position_list, i, position_1):
 	added_to_list = False
 	entry = coint_pairs.loc[coint_pairs['index'] == i]
 	#open_position_list.positions.drop(columns=['Unnamed: 0','index'])
-	entry.drop(columns=['Unnamed: 0'])
+	#entry.drop(columns=['Unnamed: 0'])
 	lg.info("Open Position List: %s" % open_position_list.positions)
 	lg.info(entry)
 	while not added_to_list:
