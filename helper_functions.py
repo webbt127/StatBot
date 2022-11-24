2a11efed6520c7686175ddf7470993a154e80619
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-23 18:01:38 -0600

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -206,7 +206,7 @@ def add_asset(coint_pairs, open_position_list, i, position_1):
 	lg.info("Open Position List: %s" % open_position_list.positions)
 	lg.info(entry)
 	while not added_to_list:
-		open_position_list.positions.append(entry)
+		open_position_list.positions = pd.concat(entry, open_position_list.positions)
 		added_to_list = True
 	lg.info(open_position_list.positions)
 	return open_position_list
