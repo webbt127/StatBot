80d522cec1f5646bf4f3482a811fadd4863af369
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-23 21:12:05 -0600

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -25,10 +25,12 @@ urllib3.disable_warnings()
 def begin_threading():
 	thread1 = Thread(target=buy_loop)
 	thread2 = Thread(target=sell_loop)
-	thread1.start()
-	time.sleep(5)
-	thread2.start()
-	time.sleep(5)
+	if api.buy:
+		thread1.start()
+		time.sleep(5)
+	if api.sell:
+		thread2.start()
+		time.sleep(5)
 	try:
 		thread1.join()
 	except Exception as e:
@@ -42,54 +44,6 @@ def begin_threading():
 		lg.info("Exception Handled in Main, Details of the Exception: %s" % e)
 		message = 'Exception Occurred: ' + e
 		send_telegram_message(message, api.telegram_chat_id, api.telegram_api_key)
-
-	
-def buy_loop():
-	while api.threaded:
-		Parallel(n_jobs=6, verbose=10, prefer="threads")(delayed(buy_loop_threaded)(i) for i in coint_pairs.index)
-	while not api.threaded:
-		wait_for_market_open()
-		for i in coint_pairs['index']:
-			position_1 = position()
-			position_1.symbol = coint_pairs['sym_1'][i]
-			position_2 = position()
-			position_2.symbol = coint_pairs['sym_2'][i]
-			
-			open_position_list.lock.acquire()
-			open_position_list_temp = open_position_list
-			open_position_list.lock.release()
-			
-			if open_position_list_temp.positions['sym_1'].str.contains(position_1.symbol).sum() == 0 and open_position_list_temp.positions['sym_1'].str.contains(position_2.symbol).sum() == 0:
-				if open_position_list_temp.positions['sym_2'].str.contains(position_1.symbol).sum() == 0 and open_position_list_temp.positions['sym_2'].str.contains(position_2.symbol).sum() == 0:
-					get_price_klines(position_1, TimeFrame.Hour, api.kline_limit)
-					get_price_klines(position_2, TimeFrame.Hour, api.kline_limit)
-					position_1.close_series = extract_close_prices(position_1)
-					position_2.close_series = extract_close_prices(position_2)
-					get_yf_info(position_1)
-					get_yf_info(position_2)
-					position_1.close_series_matched, position_2.close_series_matched = match_series_lengths(position_1, position_2)
-					if(len(position_1.close_series_matched) == len(position_2.close_series_matched) and len(position_1.close_series_matched) > 0 and position_1.quantity > 0 and position_2.quantity > 0):
-						position_1.stop_loss = round(position_1.close_series_matched[-1] * (1 - api.stop_loss_fail_safe), api.price_rounding)
-						position_2.stop_loss = round(position_2.close_series_matched[-1] * (1 - api.stop_loss_fail_safe), api.price_rounding)
-						spread_df, spread_np = calculate_spread(position_1.close_series_matched, position_2.close_series_matched, coint_pairs['hedge_ratio'][i])
-						spread_list = spread_df.astype(float).values
-						spread = spread_list[-1]
-						sma = spread_df.rolling(api.bollinger_length).mean()
-						std = spread_df.rolling(api.bollinger_length).std()
-						bollinger_up = sma + std * api.std # Calculate top band
-						bollinger_down = sma - std * api.std # Calculate bottom band
-						print_open(position_1, position_2, bollinger_up, bollinger_down, spread)
-						set_order_sides(spread, bollinger_up, bollinger_down, position_1, position_2)
-						add_asset(coint_pairs, open_position_list, i, position_1)
-						if spread > bollinger_up['spread'].iloc[-1] or spread < bollinger_down['spread'].iloc[-1]:
-							open_position_list.lock.acquire()
-							if i not in open_position_list.positions['index']:
-								initialize_order_execution(position_1)
-								initialize_order_execution(position_2)
-								message = 'Positions opened for: ' + position_1.symbol + ', ' + position_2.symbol
-								send_telegram_message(message, api.telegram_chat_id, api.telegram_api_key)
-								#add_asset(coint_pairs, open_position_list, i, position_1)
-							open_position_list.lock.release()
 							
 def buy_loop_threaded(i):
 	position_1 = position()
@@ -141,11 +95,13 @@ def sell_loop():
 		open_position_list_working = open_position_list
 		open_position_list.lock.release()
 		time.sleep(10)
-		for trade in open_position_list_working.positions['index']:
+		for trade in open_position_list_working.positions:
 			position_1 = position()
-			position_1.symbol = open_position_list_working.positions.loc[trade]['sym_1']
+			position_1.symbol = trade['sym_1']
 			position_2 = position()
-			position_2.symbol = open_position_list_working.positions.loc[trade]['sym_2']
+			position_2.symbol = trade['sym_2']
+			lg.info(position_1.symbol)
+			lg.info(position_2.symbol)
 			get_ticker_position(position_1)
 			get_ticker_position(position_2)
 			get_price_klines(position_1, TimeFrame.Hour, api.kline_limit)
