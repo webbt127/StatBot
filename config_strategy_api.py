23597465843faa6927799962665312a530d0cb04
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-23 21:37:01 -0600

    Update config_strategy_api.py

--- config_strategy_api.py
+++ config_strategy_api.py
@@ -49,9 +49,9 @@ class config:
 		self.telegram_api_key = '5558182464:AAE-d-6mNR8zNr2gxW-QxicXIbRCmB6vi6E'
 		self.threaded = True
 		self.std = 0.1
-		self.buy = True
+		self.buy = False
 		self.sell = True
-		self.use_trade_history = False
+		self.use_trade_history = True
 
 global api
 api = config()
