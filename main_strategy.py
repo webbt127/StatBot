722fd2391b35f78f3a8ffc8ea1d21ea3cb655678
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-23 21:45:49 -0600

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -99,6 +99,7 @@ def sell_loop():
 		open_position_list.lock.release()
 		time.sleep(10)
 		for trade in open_position_list_working.positions:
+			lg.info(trade)
 			position_1 = position()
 			position_1.symbol = open_position_list_working.positions.loc[trade]['sym_1']
 			position_2 = position()
