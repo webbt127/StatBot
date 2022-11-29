27a489f947e240d7793f9646855fce839e65a592
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-11-28 21:23:41 -0600

    Update main_strategy.py

--- main_strategy.py
+++ main_strategy.py
@@ -57,7 +57,7 @@ def gui_loop():
 	gui()
 		
 def buy_loop():
-	#wait_for_market_open()
+	wait_for_market_open()
 	search_size = slice(0, api.max_search, 1)
 	while api.buy:
 		Parallel(n_jobs=6, verbose=10, prefer="threads")(delayed(buy_loop_threaded)(i) for i in coint_pairs.index[search_size])
