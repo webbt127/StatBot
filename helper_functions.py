bd5615830669dc21152e9508184c5adb0ebf224b
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-12-07 01:18:53 -0600

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -339,8 +339,8 @@ def gui():
 		[graph,
 		sg.Table(values=pairs_data, headings=pairs_header_list, display_row_numbers=True, auto_size_columns=False, num_rows=min(25, len(pairs_data)), key='-PAIRDATA-', enable_click_events=True)],
 		[sg.Text(text='OPEN POSITIONS:')],
-		[sg.Table(values=positions_data, headings=positions_header_list, display_row_numbers=True, auto_size_columns=False, num_rows=min(25, len(positions_data)), key='-POSITIONDATA-', enable_click_events=True), sg.TabGroup([[sg.Tab('Bollinger Bands', bollinger_layout), sg.Tab('RSI', rsi_layout), sg.Tab('MACD', macd_layout), sg.Tab('Settings', settings_layout)]]),
-		[sg.Button('Flag'), sg.Button('Exit')]]]
+		[sg.Table(values=positions_data, headings=positions_header_list, display_row_numbers=True, auto_size_columns=False, num_rows=min(5, len(positions_data)), key='-POSITIONDATA-', enable_click_events=True), sg.TabGroup([[sg.Tab('Bollinger Bands', bollinger_layout), sg.Tab('RSI', rsi_layout), sg.Tab('MACD', macd_layout), sg.Tab('Settings', settings_layout)]])],
+		[sg.Button('Flag'), sg.Button('Exit')]]
 
 	window = sg.Window("Todd's Statistical Arbitrage Bot", layout, grab_anywhere=False)
 	while True:
