3b0a9209dd84fb88e26f8f882aa76fea68742dcc
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-12-08 23:01:58 -0600

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -371,7 +371,7 @@ def gui(coint_pairs):
 			
 		if event == '__TIMEOUT__' and not positions_df.empty:
 			window['-POSITIONDATA-'].update(values=positions_data)#, num_rows=len(positions_df.index))
-			api.bollinger_length = values['-PERIODINPUT-'][-1]
+			api.bollinger_length = values['-PERIODINPUT-']
 			print(api.bollinger_length)
 		if event == sg.WIN_CLOSED or event == 'Exit':
 			break
