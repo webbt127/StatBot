3f6212fe283880c2bbe06aa7d1c4a2056172aad7
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-12-08 19:41:10 -0600

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -472,8 +472,10 @@ def run_backtester(coint_pairs):
 				buy_price2 = None
 		profit_percent = profit_percent + pair_profit
 		print('Profit percent for ' + position_1.symbol + '/' + position_2.symbol + ': ' + str(pair_profit))
-		if pair_profit > 0:
+		if pair_profit > 0.0:
 			win_counter = win_counter + 1
+		if pair_profit < 0.0:
+			break
 		pair_profit = 0
 		print('Total profit percent: ' + str(profit_percent))
 	print('-----RESULTS-----')
